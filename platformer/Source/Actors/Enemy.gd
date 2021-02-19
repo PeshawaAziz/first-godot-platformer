9c05451fe6dbfa1a4e1c5ce8b2692854368476c3
PeshawaAziz <modern3p2017@gmail.com>
2021-02-19 15:46:52 -0500

    Add stomping ability to the Player

--- platformer/Source/Actors/Enemy.gd
+++ platformer/Source/Actors/Enemy.gd
@@ -5,6 +5,12 @@ func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
 
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	if is_on_wall():
